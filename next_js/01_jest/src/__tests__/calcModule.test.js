7edf5181c72c3e383a1715a64f907be67f87d235
SnNiFe <kimnakjun@naver.com>
2026-08-26 15:09:10 +0900

    jest 마무리

--- next_js/01_jest/src/__tests__/calcModule.test.js
+++ next_js/01_jest/src/__tests__/calcModule.test.js
@@ -4,20 +4,36 @@
 
 import {divide, minus, multiply, plus} from "@/app/calcModule";
 
-describe('사칙연산테스트',function(){
-    test('더하기 모듈 테스트',function(){
-        expect(plus(10,30)).toBe(40);
+describe('사칙연산 통합 테스트(장상,에러)',function(){
+
+    describe('사칙연산테스트',function(){
+        test('더하기 모듈 테스트',function(){
+            expect(plus(10,30)).toBe(40);
+        });
+        test('빼기 모듈 테스트',function(){
+            expect(minus(40,30)).toBe(10);
+        });
+        test('곱하기 모듈 테스트',function(){
+            expect(multiply(10,30)).toBe(300);
+        });
+        test('나누기 모듈 테스트',function(){
+            expect(divide(40,20)).toBe(2);
+        });
     });
-    test('빼기 모듈 테스트',function(){
-        expect(minus(40,30)).toBe(10);
+
+    describe('사칙연산 에러 테스트',function(){
+        test('a 보다 b 값이 클 경우',function(){
+            //throws  사용시 실행 함수를 한번 더 감싸준다.
+            expect(()=>minus(10,30)).toThrow('뺄셈의 값은 0보다 커야 합니다.');
+        });
+
+        test('0 으로 나누기 시도',function(){
+            expect(()=>divide(40,0)).toThrow('0으로 나눌 수 없습니다.');
+        });
     });
-    test('곱하기 모듈 테스트',function(){
-        expect(multiply(10,30)).toBe(300);
-    });
-    test('나누기 모듈 테스트',function(){
-        expect(divide(40,20)).toBe(2);
-    });
+
 });
+
 
 /*
 toBe()        : 숫자, 문자, 불리언 타입의 값에 일치
