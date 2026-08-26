a102da228d31db0425c3144df0ceeeba02d98565
SnNiFe <kimnakjun@naver.com>
2026-08-26 16:17:37 +0900

    나머지 사칙연산 테스트 추가

--- next_js/02_rtl/src/__tests__/calcModule.test.js
+++ next_js/02_rtl/src/__tests__/calcModule.test.js
@@ -23,4 +23,58 @@ describe('사칙연산 UI 테스트',function(){
         expect(result).toHaveTextContent('답 : 30');
     });
 
+    test('빼기 테스트', async function(){
+        // 1. UI 가져옴
+        const {container} = render(<App/>);
+        // 2. 원하는 요소 확보
+        const su1 = container.querySelector('input[name="su1"]');
+        const su2 = container.querySelector('input[name="su2"]');
+        const oper = container.querySelector('select[name="oper"]');
+        const btn = container.querySelector('button');
+        const result = screen.getByTestId('result');
+        // 3. 특정 이벤트 발생시
+        await userEvent.type(su1,'30');
+        await userEvent.type(su2,'20');
+        await userEvent.selectOptions(oper,'-');
+        await userEvent.click(btn);
+        // 4. 특정한 결과 확인
+        expect(result).toHaveTextContent('답 : 10');
+    });
+
+    test('곱하기 테스트', async function(){
+        // 1. UI 가져옴
+        const {container} = render(<App/>);
+        // 2. 원하는 요소 확보
+        const su1 = container.querySelector('input[name="su1"]');
+        const su2 = container.querySelector('input[name="su2"]');
+        const oper = container.querySelector('select[name="oper"]');
+        const btn = container.querySelector('button');
+        const result = screen.getByTestId('result');
+        // 3. 특정 이벤트 발생시
+        await userEvent.type(su1,'10');
+        await userEvent.type(su2,'20');
+        await userEvent.selectOptions(oper,'*');
+        await userEvent.click(btn);
+        // 4. 특정한 결과 확인
+        expect(result).toHaveTextContent('답 : 200');
+    });
+
+    test('나누기 테스트', async function(){
+        // 1. UI 가져옴
+        const {container} = render(<App/>);
+        // 2. 원하는 요소 확보
+        const su1 = container.querySelector('input[name="su1"]');
+        const su2 = container.querySelector('input[name="su2"]');
+        const oper = container.querySelector('select[name="oper"]');
+        const btn = container.querySelector('button');
+        const result = screen.getByTestId('result');
+        // 3. 특정 이벤트 발생시
+        await userEvent.type(su1,'20');
+        await userEvent.type(su2,'10');
+        await userEvent.selectOptions(oper,'/');
+        await userEvent.click(btn);
+        // 4. 특정한 결과 확인
+        expect(result).toHaveTextContent('답 : 2');
+    });
+
 });
