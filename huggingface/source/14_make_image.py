9f4b977685ac4ad756d999dcf25e32dca2429e1b
SnNiFe <kimnakjun@naver.com>
2026-09-04 15:35:26 +0900

    주석 수정

--- huggingface/source/14_make_image.py
+++ huggingface/source/14_make_image.py
@@ -15,7 +15,7 @@
 # # pipe = pipe.to("cuda")
 
 # prompt = "a photo of an astronaut riding a horse on mars"
-# image = pipe(prompt, num_inference_steps=12).images[0]  
+# image = pipe(prompt).images[0]  
     
 # image.save("astronaut_rides_horse.png")
 
