7378c62c4d5e886327fd10afa736d587101cd4e7
SnNiFe <kimnakjun@naver.com>
2026-09-08 15:42:15 +0900

    fast api 시작

--- fastapi/01_start/main.py
+++ fastapi/01_start/main.py
@@ -1,9 +1,9 @@
 # uv pip install fastapi uvicorn
-# uvicorn main:app --host=0.0.0.0
+# uvicorn main:app --host=0.0.0.0 --reload # host=0.0.0.0 모두 접속 허용, reload 수정 내용 자동 적용
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def main():
-    return {"message":"안녕하세요 Naak 입니다."}
+    return {"message":"안녕하세요 Naaak 입니다."}
