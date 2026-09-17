98781f7528ad1d56929b97ff5b594ec2d7dd954c
SnNiFe <kimnakjun@naver.com>
2026-09-17 14:10:48 +0900

    파이썬 올리기 기본

--- cloud/02_python.sh
+++ cloud/02_python.sh
@@ -33,5 +33,33 @@ uv pip install -r requirements.txt
 # 실행
 uvicorn main:app --host=0.0.0.0 --port=8000 --workers 2
 
+# 멈추지 않고 실행하는 방법
+# nohup : 종료되지 않고 계속 실행할수 있게 해준다.
+# > uvicorn.log 실행 내용을 uvicorn.log 로 남기겠다.
+# 2>&1 : 2는 에러로그 1은 표준출력로그 -> 에러로그도 표준출력 로그처험 출력해라
+# 2>1 로 하면 에러로그를 파일명 1에 저장하라고 오해할 수 있어 특수문자 &를 붙임
+# & : 백그라운드로 실행하라 -> 내 UI 가리지 마
+nohup uvicorn main:app --host=0.0.0.0 --port=8000 --workers 2 > uvicorn.log 2>&1 &
+
+# 로그확인 방법
+# 실시간
+tail -f uvicorn.log
+# 읽기
+cat uvicorn.log
+vim uvicorn.log
+
+# 5. 끄기
+# 8000 번 누가 사용하고 있는지?
+# LiSt Open File - 리스트에 있는거 다 열어
+# -i : Internet
+# :8000 -> :8000 이라는 문자가 나오는거
+sudo lsof -i :8000
+
+# 해당 프로세스 종료
+kill -9 [PID]
+
 # 가살환경 종료
 deactivate
+
+# main.py 파일 삭제
+rm -rf main.py
